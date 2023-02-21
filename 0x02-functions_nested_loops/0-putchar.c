c6d69d82710e8cd155dc0885f921f0453b6843b9
shaadah <saidatolamide@gmail.com>
2023-02-21 10:10:49 -0800

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include "Putchar.c"
+#include "holberton.h"
 
 /**
- * main - prints Putchar
+ * main - prints Holberton
  *
- * Decription: prints Putchar
+ * Description: prints Holberton
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	char text[10] = "Putchar";
+	char text[10] = "Holberton";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
