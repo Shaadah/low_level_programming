a841415e209719d2340079134a38a7d940ddd73f
shaadah <saidatolamide@gmail.com>
2023-02-23 16:32:24 -0800

    0. isupper

---
+++ 0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is upper
+ * @c: the number to be checked
+ * Return: 1 for upper letter 0 for any else
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 Readme
-=======
-# alx-low_level_programming
-This is a low level programming
->>>>>>> ca6a4447ca700764b2754936f71aeebf5c75f0a1
