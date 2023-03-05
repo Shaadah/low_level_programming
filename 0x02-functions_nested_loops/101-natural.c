945332ca11f93b50107b63227377ca0b36870563
shaadah <saidatolamide@gmail.com>
2023-03-05 10:09:33 -0800

    all

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 
 /**
-* main - a program that computes and prints the sum of all the multiples of 3 or 5 below 1024
+* main - computes and prints the sum of all the multiples of 3 or 5
 * Return: Always 0.
 */
 	int main(void)
